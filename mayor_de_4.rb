d66ba10eeb67cf353422c47e8047efedbde48160
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-16 20:30:03 -0300

    desafio listo. Vi todas las condiciones y posibilidades que había que hacer con cada if y eran demasiadas. Busqué en la documentación y efectivamente había algo mucho más sencillo y simple

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -1,32 +1,15 @@
-#ver arg1 arg2 arg3 y arg4
-#primero vemos si hay 3 argumentos o 4 argumentos
-#si arg1 es mayor que arg2 y arg3 y arg4 entonces arg1
-#si arg2 es mayor que arg1 y arg3 y arg4 entonces arg2
-#si arg3 es mayor que arg1 y arg2 y arg 4 entonces arg3
-#si arg4 es mayor que arg1 y arg2 y arg 3 entonces arg4
-
-input1 = ARGV[0]
-input2 = ARGV[1]
-input3 = ARGV[2]
-input4 = ARGV[3]
+input1 = ARGV[0].to_i
+input2 = ARGV[1].to_i
+input3 = ARGV[2].to_i
+input4 = ARGV[3].to_i
 
 
-if ARGV.length == 3
-  if input1 > input2 && input1 > input2
-    puts input1
-  elsif input2 > input1 && input2 > input3
-    puts input2
-  else input3 > input1 && input3 > input2
-    puts input3
-  end
-else ARGV.length == 4
-  if input1 > input2 && input1 > input2 && input1 > input4
-    puts input1
-  elsif input2 > input1 && input2 > input3 && input2 > input4
-    puts input2
-  elsif input3 > input1 && input3 > input2 && input3 > input4
-    puts input3
-  else input4 > input1 && input4 > input2 && input4 > input3
-    puts input4
-  end
+inputs = ARGV.length
+
+if inputs == 4
+  puts [input1, input2, input3, input4].max
+elsif inputs == 3
+  puts [input1, input2, input3].max
+else
+  puts "Debes ingresar 3 o 4 números. Nada más y nada menos."
 end
