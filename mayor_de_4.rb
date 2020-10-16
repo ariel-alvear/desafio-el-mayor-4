4f412021820678f2d2fd92cd98a03d906db72c46
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-16 19:55:02 -0300

    avanzando

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -1,4 +1,5 @@
 #ver arg1 arg2 arg3 y arg4
+#primero vemos si hay 3 argumentos o 4 argumentos
 #si arg1 es mayor que arg2 y arg3 y arg4 entonces arg1
 #si arg2 es mayor que arg1 y arg3 y arg4 entonces arg2
 #si arg3 es mayor que arg1 y arg2 y arg 4 entonces arg3
@@ -9,12 +10,23 @@ input2 = ARGV[1]
 input3 = ARGV[2]
 input4 = ARGV[3]
 
-if input1 > input2 && input1 > input2 && input1 > input4
-  puts input1
-elsif input2 > input1 && input2 > input3 && input2 > input4
-  puts input2
-else input3 > input1 && input3 > input2 && input3 > input4
-  puts input3
-else input4 > input1 && input4 > input2 && input4 > input3
-  puts input4
+
+if ARGV.length == 3
+  if input1 > input2 && input1 > input2
+    puts input1
+  elsif input2 > input1 && input2 > input3
+    puts input2
+  else input3 > input1 && input3 > input2
+    puts input3
+  end
+else ARGV.length == 4
+  if input1 > input2 && input1 > input2 && input1 > input4
+    puts input1
+  elsif input2 > input1 && input2 > input3 && input2 > input4
+    puts input2
+  elsif input3 > input1 && input3 > input2 && input3 > input4
+    puts input3
+  else input4 > input1 && input4 > input2 && input4 > input3
+    puts input4
+  end
 end
